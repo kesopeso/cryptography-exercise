d94d3c33daa1b6f86c78eb99e6fc594935e2f3b5
Dorde Kesic <dordekesic.keso@gmail.com>
2026-03-11 02:08:31 +0100

    adds basic Encode bitset function

--- internal/bitset/bitset.go
+++ internal/bitset/bitset.go
@@ -1,6 +1,9 @@
 package bitset
 
 import (
+	"bytes"
+	"compress/gzip"
+	"encoding/base64"
 	"fmt"
 
 	"github.com/kesopeso/cryptography-exercise/internal/assert"
@@ -56,4 +59,22 @@ func (b *Bitset) Set(index int, value bool) error {
 	}
 
 	return nil
+}
+
+// Encode returns the gzipped, base64-encoded string of the byte array
+func (b *Bitset) Encode() (string, error) {
+	var buf bytes.Buffer
+
+	gz := gzip.NewWriter(&buf)
+
+	_, err := gz.Write(b.data)
+	if err != nil {
+		return "", err
+	}
+
+	if err := gz.Close(); err != nil {
+		return "", err
+	}
+
+	return base64.StdEncoding.EncodeToString(buf.Bytes()), nil
 }
