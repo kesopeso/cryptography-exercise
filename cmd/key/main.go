49d5b4c1d428e68c497ff45d2ff05a9f824f5088
Dorde Kesic <dordekesic.keso@gmail.com>
2026-03-14 09:05:41 +0100

    refactors cmd files to be more idiomatic

--- cmd/key/main.go
+++ cmd/key/main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"os"
 
 	"github.com/kesopeso/cryptography-exercise/pkg/cryptography"
 )
 
 func main() {
+	if err := run(); err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, "error:", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
 	err := cryptography.GenerateAndSaveECDSAKey("server.pem")
 	if err != nil {
-		log.Fatalf("failed generating key: %v", err)
+		return fmt.Errorf("failed generating key: %v", err)
 	}
 
 	fmt.Println("key generated")
+	return nil
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"net/http"
+	"os"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/kesopeso/cryptography-exercise/internal/server"
@@ -13,12 +13,19 @@ import (
 )
 
 func main() {
+	if err := run(); err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, "error:", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
 	cfg := loadConfig()
 	ctx := context.Background()
 
 	conn, err := pgx.Connect(ctx, cfg.dbURL)
 	if err != nil {
-		log.Fatalf("failed to connect to database: %v", err)
+		return fmt.Errorf("failed to connect to database: %v", err)
 	}
 	defer conn.Close(ctx)
 
@@ -28,11 +35,12 @@ func main() {
 
 	httpServer := server.NewHttpServer(&http.Server{Addr: cfg.addr, Handler: handler})
 
-	fmt.Printf("server listening on %s\n", cfg.addr)
+	fmt.Println("server listening on", cfg.addr)
 
 	if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		log.Fatalf("server shutdown error: %v", err)
+		return fmt.Errorf("server shutdown error: %v", err)
 	}
 
 	fmt.Println("server closed")
+	return nil
 }
